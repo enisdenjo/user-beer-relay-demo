8128221f305bf6a6d4c3924ba04e8727c7004756
enisdenjo <badurinadenis@gmail.com>
2021-09-28 16:25:06 +0200

    add react alpha
    yarn add react@alpha react-dom@alpha

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = document.getElementById('root') as HTMLElement;
+ReactDOM.createRoot(root).render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
